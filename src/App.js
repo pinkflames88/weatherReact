a8ae2a6c0279cc5b34072624e182e9ab46bee206
pinkflames88 <xnaomiGx@gmail.com>
2023-02-27 22:35:14 +0000

    updates made

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Weather from "./WeatherSearch.js";
+import "./App.css";
+import "./Weather.css";
+import "./WeatherSearch.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Weatherley </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
