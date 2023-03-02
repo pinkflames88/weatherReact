a075af04566190a18fc383cb5c6a82264af0f97a
pinkflames88 <xnaomiGx@gmail.com>
2023-03-02 00:05:19 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Weather from "./WeatherSearch.js";
 import "./App.css";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
-import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Milan" />
+
+        <footer>
+          <a
+            href="https://github.com/pinkflames88/WeatherReact"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+             <br> 
+            Designed by Naomi G </br>
+         <div class="card-footer"> 
+         </dov>
+         </footer>
+    
+
+
+      </div>
+    </div>
+  );
+}
